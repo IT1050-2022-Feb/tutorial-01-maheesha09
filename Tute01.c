606f8183ef7d07c8dd845f4ddec1fe89daaf24b2
maheesha09 <95403889+maheesha09@users.noreply.github.com>
2022-02-13 22:26:24 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+   
+    int mark1,mark2;
+    float avg;
+
+    printf("Enter mark 1 : ");
+    scanf("%d",&mark1);
+
+    printf("Enter mark 2 : ");
+    scanf("%d",&mark2);
+
+    avg = (mark1 + mark2)/2.0;
+    printf("Average of the two marks : %.2f",avg);
+
+    return 0;
 }
+  
+
 
